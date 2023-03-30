e9dd4075ccb34d4507c1ba09525182f9fafdf95c
Nathaniel81 <nathanield356@gmail.com>
2023-03-30 19:56:14 +0300

    Basic annotations - add

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,3 +1,7 @@
 #!/usr/bin/env python3
+"""Provides a function for performing add operation"""
+
+
 def add(a: float, b: float) -> float:
+    """Return the sum of two floating-point numbers."""
     return a + b
