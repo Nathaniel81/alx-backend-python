e84723179218727c1f5e0cd4b7fc5558d0a7c1fc
Nathaniel81 <nathanield356@gmail.com>
2023-03-30 20:15:16 +0300

    Basic annotations - floor

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 """Provides a function for calculating the floor of a floating-point number"""
-
-
 import math
+
 
 def floor(n: float) -> int:
     """Calculates the floor of a floating-point number"""
