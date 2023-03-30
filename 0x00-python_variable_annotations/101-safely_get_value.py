776517fa6c41d80fe4bd7a95f17d121813f36c82
Nathaniel81 <nathanield356@gmail.com>
2023-03-30 22:16:16 +0300

    More involved type annotations

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+Returns the value associated with a given key in a dictionary,
+or a default value if the key is not present in the dictionary
+"""
+from typing import Mapping, Any, TypeVar, Union
+
+T = TypeVar('T')
+
+
+
+def safely_get_value(dct: Mapping, key: Any, default: Union[T, None] = None) -> Union[Any, T]:
+    """
+    Returns the value associated with the key in the dictionary,
+    or the default value if the key is not present in the dictionary.
+    If a default value is provided,
+    the returned value will have the same type as the default value
+    """
+    if key in dct:
+        return dct[key]
+    else:
+        return default
